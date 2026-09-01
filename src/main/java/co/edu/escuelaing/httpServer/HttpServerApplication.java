f740934674f2b695febb2f851445b2715b4be1df
Daniel Ahumada <danielahumada071@gmail.com>
2026-09-01 08:43:29 -0500

    create maven project

---
+++ src/main/java/co/edu/escuelaing/httpServer/HttpServerApplication.java
@@ -0,0 +1,13 @@
+package co.edu.escuelaing.httpServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpServerApplication.class, args);
+	}
+
+}
