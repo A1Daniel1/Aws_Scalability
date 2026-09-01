9deed9f20cf2b6df348d72cc2c349134449beece
Daniel Ahumada <danielahumada071@gmail.com>
2026-09-01 10:47:29 -0500

    http server implementation

--- src/main/java/co/edu/escuelaing/httpServer/URLReader.java
+++ src/main/java/co/edu/escuelaing/httpServer/URLReader.java
@@ -2,12 +2,35 @@ package co.edu.escuelaing.httpServer;
 
 import java.io.*;
 import java.net.*;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class URLReader {
     public static void main(String[] args) throws Exception {
-        URL google = new URL("http://www.google.com/");
+        URL siteURL = new URL("https://www.google.com/");
+        
+        URLConnection connection = siteURL.openConnection();
+
+
+        // reading headersss
+        Map<String, List<String>> headers = connection.getHeaderFields();
+        Set<Map.Entry<String, List<String>>> entries = headers.entrySet();
+
+        for (Map.Entry<String, List<String>> entry : entries) {
+            String headerName = entry.getKey();
+            // A null name represents the HTTP status line.
+            if (headerName != null)
+                System.out.print(headerName + ":");
+            for (String value : entry.getValue())
+                System.out.print(value);
+            System.out.println();
+        }
+
+
+        // the old method
         try (BufferedReader reader = new BufferedReader(
-                new InputStreamReader(google.openStream()))) {
+                new InputStreamReader(connection.getInputStream()))) {
             String inputLine;
             while ((inputLine = reader.readLine()) != null) {
                 System.out.println(inputLine);
